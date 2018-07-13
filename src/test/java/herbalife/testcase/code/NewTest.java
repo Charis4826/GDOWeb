82e642e27bb800a2a9378a376e8123a02e1ea8df
Charis <zsh_4826@163.com>
2018-07-13 17:08:30 +0800

    add ch

--- src/test/java/herbalife/testcase/code/NewTest.java
+++ src/test/java/herbalife/testcase/code/NewTest.java
@@ -1,13 +1,18 @@
 package herbalife.testcase.code;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -16,11 +21,45 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
-	 private WebDriver driver;
-	 protected DesiredCapabilities capabilities;
+	 WebDriver driver;
+	 DesiredCapabilities capabilities;
+	 
+	 @BeforeTest
+	 @Parameters({ "parameter"})
+	  public void beforeTest(String dbconfig) throws Exception {
+		 Properties prop = new Properties();
+		 prop.load(new FileInputStream(dbconfig));  
+		 
+		 String drivertype = prop.getProperty("drivertype");
+         String driverdetail = prop.getProperty("driverdetail");
+         String browser = prop.getProperty("browser");
+         
+	      System.setProperty(drivertype, getClass().getResource(driverdetail).toURI().getPath());
+	      
+	      if (browser.toUpperCase().equals("IE")) {
+	    	  driver = new InternetExplorerDriver();
+	      }else if (browser.toUpperCase().equals("CHROME")) {
+	    	  	capabilities = DesiredCapabilities.chrome();
+	      		capabilities.setBrowserName(browser);
+	      		ChromeOptions options = new ChromeOptions();
+	            //通过配置参数进制data;的出现
+	            options.addArguments(
+	          		  "--user-data-dir=C:/Users/Administrator/AppData/Local/Google/Chrome/User Data/Default");
+	            //通过配置参数删除“您使用的是不受支持的命令行标记：--ignore-certificate-errors.稳定性和安全性会有所下降。”的提示
+	            options.addArguments("--start-maximized","allow-running-insecure-content","--test-type");
+	            capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+	            driver = new ChromeDriver(capabilities);
+	      	}else if (browser.toUpperCase().equals("FIREFOX")) {
+	      			driver = new FirefoxDriver();
+	      		}
+	     
+	      driver.manage().window().maximize();
+	  }
+	 
 	 
   @Test
   public void open() {
@@ -35,20 +74,7 @@ public class NewTest {
       driver.findElement(inputBox).sendKeys("中国地图");
       driver.findElement(searchButton).click();
   }
-  @BeforeTest
-  public void beforeTest() throws Exception {
-	  capabilities = DesiredCapabilities.chrome();
-      capabilities.setBrowserName("ie");
-      
-      
-      System.setProperty("webdriver.ie.driver", getClass().getResource("/IEDriverServer.exe").toURI().getPath());
-      
-      System.out.println(getClass().getResource("/IEDriverServer.exe").toURI().getPath());
-      //System.setProperty("webdriver.ie.driver", "F:\\herbalife\\GDOWeb\\src\\main\\resources\\IEDriverServer.exe");
-      driver = new InternetExplorerDriver(capabilities);
-      driver.manage().window().maximize();
-  }
-
+ 
 @AfterTest
   public void afterTest() {
 	  try {
